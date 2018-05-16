efa4f97627b70ccba2f98574090abd751bc11d83
Shulhan <ms@kilabit.info>
2018-05-17 06:12:59 +0700

    [test] Move test initialization and common test functions to beku_test

---
+++ beku_test.go
@@ -0,0 +1,107 @@
+package beku
+
+import (
+	"go/build"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+const (
+	testGitRepo = "github.com/shuLhan/beku_test"
+)
+
+var (
+	testEnv    *Env
+	gitCurPkg  *Package
+	gitNewPkg  *Package
+	testStdout *os.File
+	testStderr *os.File
+)
+
+func testInitOutput() (err error) {
+	testStdout, err = ioutil.TempFile("", "")
+	if err != nil {
+		return
+	}
+
+	testStderr, err = ioutil.TempFile("", "")
+	if err != nil {
+		return
+	}
+
+	defStdout = testStdout
+	defStderr = testStderr
+
+	return
+}
+
+func testGetOutput(t *testing.T) (stdout, stderr string) {
+	bout, err := ioutil.ReadAll(defStdout)
+	if err != nil {
+		t.Fatal(err)
+	}
+	berr, err := ioutil.ReadAll(testStderr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	stdout = string(bout)
+	stderr = string(berr)
+
+	return
+}
+
+func testResetOutput(t *testing.T, truncate bool) {
+	_, err := testStdout.Seek(0, io.SeekStart)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = testStderr.Seek(0, io.SeekStart)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if truncate {
+		testStdout.Truncate(0)
+		testStderr.Truncate(0)
+	}
+}
+
+func TestMain(t *testing.M) {
+	orgGOPATH := build.Default.GOPATH
+
+	testGOPATH, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testGOPATH += "/testdata"
+	build.Default.GOPATH = testGOPATH
+
+	defer func() {
+		build.Default.GOPATH = orgGOPATH
+	}()
+
+	err = testInitOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testEnv, err = NewEnvironment()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gitCurPkg = NewPackage(testGitRepo, testGitRepo, VCSModeGit)
+	gitNewPkg = NewPackage(testGitRepo, testGitRepo, VCSModeGit)
+
+	log.Printf("test env : %+v\n", *testEnv)
+	log.Printf("gitCurPkg: %+v\n", *gitCurPkg)
+	log.Printf("gitNewPkg: %+v\n", *gitNewPkg)
+
+	os.Exit(t.Run())
+}
--- package_test.go
+++ package_test.go
@@ -85,3 +85,101 @@ func TestIsEqual(t *testing.T) {
 		test.Assert(t, "", c.exp, got, true)
 	}
 }
+
+func TestAddDep(t *testing.T) {
+	cases := []struct {
+		desc           string
+		envPkgs        []*Package
+		importPath     string
+		exp            bool
+		expDeps        []string
+		expDepsMissing []string
+		expPkgsMissing []string
+	}{{
+		desc:       "Is the same path as package",
+		importPath: testGitRepo,
+	}, {
+		desc:       "Is vendor package",
+		importPath: "vendor/github.com/shuLhan/beku",
+	}, {
+		desc:       "Is standard package",
+		importPath: "os/exec",
+	}, {
+		desc: "Is exist on environment",
+		envPkgs: []*Package{{
+			ImportPath: "github.com/shuLhan/beku",
+		}, {
+			ImportPath: "github.com/shuLhan/share",
+		}},
+		importPath: "github.com/shuLhan/share/lib/test",
+		exp:        true,
+		expDeps: []string{
+			"github.com/shuLhan/share",
+		},
+	}, {
+		desc: "Is exist on environment (again)",
+		envPkgs: []*Package{{
+			ImportPath: "github.com/shuLhan/beku",
+		}, {
+			ImportPath: "github.com/shuLhan/share",
+		}},
+		importPath: "github.com/shuLhan/share/lib/test",
+		exp:        true,
+		expDeps: []string{
+			"github.com/shuLhan/share",
+		},
+	}, {
+		desc:       "Is not exist on environment (missing)",
+		importPath: "github.com/shuLhan/tekstus",
+		exp:        true,
+		expDeps: []string{
+			"github.com/shuLhan/share",
+		},
+		expDepsMissing: []string{
+			"github.com/shuLhan/tekstus",
+		},
+		expPkgsMissing: []string{
+			"github.com/shuLhan/tekstus",
+		},
+	}, {
+		desc:       "Is not exist on environment (again)",
+		importPath: "github.com/shuLhan/tekstus",
+		exp:        true,
+		expDeps: []string{
+			"github.com/shuLhan/share",
+		},
+		expDepsMissing: []string{
+			"github.com/shuLhan/tekstus",
+		},
+		expPkgsMissing: []string{
+			"github.com/shuLhan/tekstus",
+		},
+	}}
+
+	var got bool
+
+	gitCurPkg.Deps = nil
+	gitCurPkg.DepsMissing = nil
+
+	for _, c := range cases {
+		t.Log(c.desc)
+
+		testEnv.pkgs = c.envPkgs
+		testEnv.pkgsMissing = nil
+		got = gitCurPkg.addDep(testEnv, c.importPath)
+
+		test.Assert(t, "return", c.exp, got, true)
+
+		if !got {
+			continue
+		}
+
+		test.Assert(t, "Deps", c.expDeps, gitCurPkg.Deps, true)
+		test.Assert(t, "DepsMissing", c.expDepsMissing, gitCurPkg.DepsMissing, true)
+		test.Assert(t, "env.pkgsMissing", c.expPkgsMissing,
+			testEnv.pkgsMissing, true)
+	}
+
+	gitCurPkg.Deps = nil
+	gitCurPkg.DepsMissing = nil
+}
